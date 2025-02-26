c04d8e21b7550811d4383c3fcb58d01e7568edcc
Cazazazoo <zhuconnie123@csu.fullerton.edu>
2025-02-25 19:25:52 -0800

    Initial Commit

---
+++ gwish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gwish
+//
+//  Created by Connie Zhu on 2/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gwish/gwishApp.swift
@@ -0,0 +1,17 @@
+//
+//  gwishApp.swift
+//  gwish
+//
+//  Created by Connie Zhu on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct gwishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
