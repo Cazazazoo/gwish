bf72b6089c061e93f5cc91659ba81aa531b24b50
Cazazazoo <zhuconnie123@csu.fullerton.edu>
2025-05-16 20:42:02 -0700

    update wishlist details er

---
+++ gwish/Views/ProfileWishlistView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileWishlistView.swift
+//  gwish
+//
+//  Created by Connie Zhu on 5/16/25.
+//
+
+import SwiftUI
+
+struct ProfileWishlistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileWishlistView()
+}
